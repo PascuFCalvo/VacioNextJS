0619c9e3eda403c4789ab667efa546c336a2b4d4
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-02-06 16:17:31 +0100

    video-audio

--- src/app/mainSection/page.jsx
+++ src/app/mainSection/page.jsx
@@ -10,6 +10,7 @@ export default function LandinPageSection4() {
           className="w-full h-auto min-w-full min-h-[350px] md:min-h-full object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
           src={require("../../../public/assets/teaser3.mp4")}
           autoPlay
+          muted
         />
       </Suspense>
     </div>
