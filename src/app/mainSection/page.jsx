3aae1e21f693227d32e24930380078c0260c6acf
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-02-06 16:15:30 +0100

    video-audio

--- src/app/mainSection/page.jsx
+++ src/app/mainSection/page.jsx
@@ -1,18 +1,17 @@
+import React, { Suspense } from "react";
 import Navbar from "../navbar/navbar";
 
 export default function LandinPageSection4() {
   return (
-    <>
-      <div className="relative h-screen overflow-hidden bg-neutral-900">
-        <Navbar />
+    <div className="relative h-screen overflow-hidden bg-neutral-900">
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
         <video
-          className="w-full h-auto min-h-[350px] md:min-w-full md:min-h-full object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          className="w-full h-auto min-w-full min-h-[350px] md:min-h-full object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
           src={require("../../../public/assets/teaser3.mp4")}
           autoPlay
-          muted={false}
-          loop
         />
-      </div>
-    </>
+      </Suspense>
+    </div>
   );
 }
