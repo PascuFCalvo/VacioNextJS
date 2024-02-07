655b649adea255c696075069facd3c845d9324a1
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-02-07 11:37:02 +0100

    performance: set Image element on img

--- src/app/galeria/gallery.jsx
+++ src/app/galeria/gallery.jsx
@@ -1,25 +1,25 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 
 export default function Gallery() {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-10 mx-4">
       <div className="grid gap-4">
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan1.jpg"
             alt=""
           />
         </div>
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan2.jpg"
             alt=""
           />
         </div>
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan3.jpg"
             alt=""
@@ -28,21 +28,21 @@ export default function Gallery() {
       </div>
       <div className="grid gap-4">
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan4.jpg"
             alt=""
           />
         </div>
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan5.jpg"
             alt=""
           />
         </div>
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan6.jpg"
             alt=""
@@ -51,21 +51,21 @@ export default function Gallery() {
       </div>
       <div className="grid gap-4">
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan7.jpg"
             alt=""
           />
         </div>
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan8.jpg"
             alt=""
           />
         </div>
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan9.jpg"
             alt=""
@@ -74,21 +74,21 @@ export default function Gallery() {
       </div>
       <div className="grid gap-4">
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan10.jpg"
             alt=""
           />
         </div>
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan11.jpg"
             alt=""
           />
         </div>
         <div>
-          <img
+          <Image
             className="rounded-lg object-cover w-full h-full"
             src="../../../assets/juan12.jpg"
             alt=""
