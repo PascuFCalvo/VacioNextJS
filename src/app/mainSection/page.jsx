12134f7595b364d4401876c43ca6c7533ba8360e
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-02-07 16:32:09 +0100

    feat: fixing embed video height

--- src/app/mainSection/page.jsx
+++ src/app/mainSection/page.jsx
@@ -3,11 +3,11 @@ import Navbar from "../components/Navbar";
 
 export default function MainPage() {
   return (
-    <div className="relative h-screen overflow-hidden bg-neutral-900">
+    <div className="relative h-screen md:h-[1050px] overflow-hidden bg-neutral-900">
       <Navbar />
       <Suspense fallback={<div>Loading...</div>}>
         <video
-          className="w-full h-auto min-w-full min-h-[350px] md:max-h-screen md:min-h-full object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          className="w-full h-auto min-w-full min-h-[350px] md:max-h-full md:min-h-full object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
           src={require("../../../public/assets/teaser3.mp4")}
           autoPlay
           muted
