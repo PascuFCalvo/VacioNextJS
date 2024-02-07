583f93169b95e3ce4a27366af4dfcf781d2b5efa
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-02-07 11:02:39 +0100

    fix: adding  navbar to test deploy

--- src/app/mainSection/page.jsx
+++ src/app/mainSection/page.jsx
@@ -1,8 +1,10 @@
 import React, { Suspense } from "react";
+import Navbar from "../navbar/Navbar";
 
 export default function LandinPageSection4() {
   return (
     <div className="relative h-screen overflow-hidden bg-neutral-900">
+      <Navbar />
       <Suspense fallback={<div>Loading...</div>}>
         <video
           className="w-full h-auto min-w-full min-h-[350px] md:min-h-full object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
