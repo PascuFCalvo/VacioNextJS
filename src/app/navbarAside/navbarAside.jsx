6a25443c5f058bebbdb225790e923282ab4d8de9
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-02-07 13:20:10 +0100

    adjust navbar side

--- src/app/navbarAside/navbarAside.jsx
+++ src/app/navbarAside/navbarAside.jsx
@@ -12,24 +12,26 @@ import "./transition.css";
 export default function NavbarAside() {
   return (
     <div className="fixed top-0 h-screen right-0 bg-neutral-100/20 flex flex-col items-center justify-center pb-5 pt-5 z-50 lg:mt-[66px]">
-      <Link href="https://www.instagram.com/dualcillo/" target="_blank">
-        <Instagram className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl hover: " />
-      </Link>
-      <Link href="https://twitter.com/dualcillo" target="_blank">
-        <X className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl" />
-      </Link>
-      <Link
-        href="https://www.linkedin.com/in/juan-dual-mateo-b6277695/"
-        target="_blank"
-      >
-        <LinkedIn className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl" />
-      </Link>
-      <Link href="https://www.tiktok.com/@dualcillo?lang=es" target="_blank">
-        <MusicNote className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl" />
-      </Link>
-      <Link href="https://www.youtube.com/@RunnifeJuanDual" target="_blank">
-        <YouTube className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl" />
-      </Link>
+      <div className="flex flex-col mt[-66px]">
+        <Link href="https://www.instagram.com/dualcillo/" target="_blank">
+          <Instagram className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl lg:mt-[66px] " />
+        </Link>
+        <Link href="https://twitter.com/dualcillo" target="_blank">
+          <X className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl" />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/juan-dual-mateo-b6277695/"
+          target="_blank"
+        >
+          <LinkedIn className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl" />
+        </Link>
+        <Link href="https://www.tiktok.com/@dualcillo?lang=es" target="_blank">
+          <MusicNote className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl" />
+        </Link>
+        <Link href="https://www.youtube.com/@RunnifeJuanDual" target="_blank">
+          <YouTube className="social-icon text-3xl text-neutral-900 m-2 hover:text-5xl" />
+        </Link>
+      </div>
     </div>
   );
 }
